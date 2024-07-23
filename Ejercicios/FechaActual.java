4f9a3b349c2fd473c558469f4f5467fa9ba71fad
Antoniocr7 <cortezantonio124@gmail.com>
2024-07-23 18:58:42 +0000

    La Fecha de Hoy es:

---
+++ Ejercicios/FechaActual.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class FechaActual {
+    public static void main(String[] args) {
+        LocalDate fechaActual = LocalDate.now();
+        System.out.println("La Fecha de hoy es: " + fechaActual);
+        }    
+}
