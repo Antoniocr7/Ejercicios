3212e3e97e57bf6fe347be697c10fbb4ae2d731a
Antoniocr7 <cortezantonio124@gmail.com>
2024-07-24 17:58:41 +0000

    Tabla de Multiplicar Infinito:

--- Ejercicios/TablaMultiplicar.java
+++ Ejercicios/TablaMultiplicar.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TablaMutiplicar {
+public class TablaMultiplicar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String respuesta;
